02dea7238f9684560af558d6a1c05454a56fc39d
YashGoswamiBIAS <100066481+YashGoswamiBIAS@users.noreply.github.com>
2022-09-30 23:43:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import {Display} from "./comp/display"
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React <Display />
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/comp/display.js
@@ -0,0 +1,8 @@
+import {React} from 'react'
+
+export function Display()
+{
+    return (
+        <div>Hello</div>
+    )
+}
